801d319c196706ff3650e7b21d75cb717a3b99bb
Amitava <amitavagc@gmail.com>
2021-07-30 17:04:53 +0530

    class free code

---
+++ pythonapp2021/main.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+app = Falsk(__name__)
+@app.route('/')
+def  index():
+	return  "welcome To python Application using flask"
+
+if __name__ == '__main__':
+	app.run(debug=True,host='0.0.0.0',port=8080)
